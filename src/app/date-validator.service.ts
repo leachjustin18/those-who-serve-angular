906f80fd42fa93d9f6b0af6e92cd6d4f7ee47478
Justin Leach <justin.leach@Justins-MacBook-Pro.local>
2020-02-20 16:33:59 -0600

    Servants
    Add date validator services

---
+++ src/app/date-validator.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DateValidatorService {
+  usaDate(control: FormControl): { [key: string]: boolean } {
+    const unitedStateDatePatter = /^(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](20|21)\d\d$/;
+
+    if (!control.value) {
+      return;
+    }
+
+    if (!control.value.match(unitedStateDatePatter)) {
+      return { usaDateError: true };
+    }
+
+    return null;
+  }
+
+  lessThenToday(control: FormControl): { [key: string]: boolean } {
+    const today = new Date();
+    const controlDate = new Date(control.value);
+
+    if (!control.value) {
+      return;
+    }
+
+    if (today >= controlDate) {
+      return { lessThenToday: true };
+    }
+
+    return null;
+  }
+}
