1647aa16dc2af0acd737159e116e81d8cfaacd39
Justin Leach <leachjustin18@yahoo.com>
2020-12-22 18:22:24 -0600

    Add create schedule components

--- src/app/create-schedule/create-schedule.component.ts
+++ src/app/create-schedule/create-schedule.component.ts
@@ -1,12 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+type TDate = {
+  value: Date;
+  viewValue: string;
+};
+
 @Component({
   selector: 'app-create-schedule',
   templateUrl: './create-schedule.component.html',
   styleUrls: ['./create-schedule.component.scss']
 })
 export class CreateScheduleComponent implements OnInit {
-  constructor() {}
+  dates: TDate[] = [];
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.generateNext4Months();
+  }
+
+  generateNext4Months() {
+    const now = new Date();
+
+    for (let i = 1; i < 5; i++) {
+      const date = new Date(now.getFullYear(), now.getMonth() + i, 1);
+      this.dates.push({
+        value: date,
+        viewValue: `${date.toLocaleString('default', {
+          month: 'long'
+        })} ${date.getFullYear()}`
+      });
+    }
+  }
 }
